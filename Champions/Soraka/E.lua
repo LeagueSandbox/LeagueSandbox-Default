c3ab2805ee0e5f62d8d75f87491c688fe8cb9043
FurkanS1821 <furkan.sam.1821@gmail.com>
2016-10-29 11:44:48 +0300

    Content Update

---
+++ Champions/Soraka/E.lua
@@ -0,0 +1,17 @@
+Vector2 = require 'Vector2' -- include 2d vector lib
+
+function onStartCasting()
+
+end
+
+function onFinishCasting()
+
+end
+
+function applyEffects()
+
+end
+
+function onUpdate(diff)
+
+end
