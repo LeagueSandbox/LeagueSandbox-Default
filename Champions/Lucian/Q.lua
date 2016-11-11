303d1145bc54456f1eb05413d8e2a3e209968eaf
DeudlyYT <deudlymandame@gmail.com>
2016-11-11 22:25:13 +0100

    Lucian Q working
    Lucian Q working

--- Champions/Lucian/Q.lua
+++ Champions/Lucian/Q.lua
@@ -1,12 +1,27 @@
 Vector2 = require 'Vector2' -- include 2d vector lib 
 
-function onFinishCasting()
-
-	-- THIS IS A LASER SPELL TYPE BUT THE API DOESNT HAVE LASERS
+function onStartCasting()
+	local current = Vector2:new(owner.X, owner.Y)
+    local to = (Vector2:new(spell.X, spell.Y) - current):normalize()
+    local range = to * 1100
+    local trueCoords = current + range
 	
-	-- SO THIS SPELL CANNOT BE CREATED BY THE MOMENT
+	addLaser(trueCoords.x, trueCoords.y, true)
+	spellAnimation("SPELL1", owner)
+	addParticle(owner, "Lucian_Q_laser.troy", trueCoords.x, trueCoords.y)
+	addParticleTarget(owner, "Lucian_Q_tar.troy", owner)
 
 end
+function onFinishCasting()
+
+end
+
 function applyEffects()
+	local AD = (owner:GetStats().AttackDamage.Total*(0.45 + (spellLevel*0.15))) + (50 + (spellLevel*30))
+	dealPhysicalDamage(AD)
+	
+end
+
+function onUpdate(diff)
 
 end
