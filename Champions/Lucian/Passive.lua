f1ff678b7e039d0bad8719cac276c449378ecf45
DeudlyYT <deudlymandame@gmail.com>
2016-11-13 16:58:02 +0100

    New content (#3)
    Improvements to Annie, Blitzcrank, Ezreal, Gangplank, Lucian and Lux
    
    Annie:
    * Add Q
    
    Blitzcrank:
    * Add Q
    
    Ezreal:
    * Add Q base
    
    Gangplank:
    * Add Q
    * Add W
    * Update Passive
    * Update Ult
    
    Lucian:
    * Add Q
    * Update W
    * Update and fix R
    * Update Passive
    
    Lux:
    * Fix R

--- Champions/Lucian/Passive.lua
+++ Champions/Lucian/Passive.lua
@@ -1,11 +1,25 @@
+Vector2 = require 'Vector2' -- include 2d vector lib
+
 function onUpdate(diff)
-    --print("Onupdate")
+
 end
 
-function onDamageTaken(attacker, damage, dmgType, source)
-    print("Damage taken is ".. damage)
+function onDamageTaken(attacker, damage, type, source)
+
 end
 
-function onAutoAttack(us, target)
-    print("We just auto attacked!")
+function onAutoAttack(target)
+
+end
+
+function onDealDamage(target, damage, type, source)
+
+end
+
+function onSpellCast(x, y, slot, target)
+
+end
+
+function onDie(killer)
+
 end
