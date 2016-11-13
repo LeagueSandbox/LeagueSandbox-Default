d8ee6ca2e7fb1b13ab0b988ab47c0b1646cee5e4
DeudlyYT <deudlymandame@gmail.com>
2016-11-13 16:14:46 +0100

    Ezreal Q formula

--- Champions/Ezreal/Q.lua
+++ Champions/Ezreal/Q.lua
@@ -16,7 +16,7 @@ end
 function applyEffects()
     local AD = owner:GetStats().AttackDamage.Total*1.1
     local AP = owner:GetStats().AbilityPower.Total*0.4
-    local damage = getEffectValue(0) + AD + AP
+    local damage = 15 + (spellLevel*20) + AD + AP
     owner:dealDamageTo(getTarget(), damage, TYPE_PHYSICAL, SOURCE_ATTACK, false)
     lowerCooldown(0, 1)
     lowerCooldown(1, 1)
