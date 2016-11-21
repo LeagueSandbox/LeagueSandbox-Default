9c9c489d80e399ad469de53abfe9e0d504ec5cb7
FurkanS1821 <furkan.sam.1821@gmail.com>
2016-11-21 20:19:06 +0300

    Fixed spawnpoints and some scripts Deudly wrote.

--- Champions/Blitzcrank/Q.lua
+++ Champions/Blitzcrank/Q.lua
@@ -1,7 +1,7 @@
 Vector2 = require 'Vector2' -- include 2d vector lib 
 
 function onStartCasting()
-    addParticleTarget(owner, "Blitzcrank_Grapplin_tar.troy", owner, 1, "L_HAND")
+    spellAnimation("SPELL1", owner)
 end
 
 function onFinishCasting()
@@ -10,7 +10,6 @@ function onFinishCasting()
     local range = to * 925
     local trueCoords = current + range
     addProjectile("RocketGrabMissile", trueCoords.x, trueCoords.y)
-    spellAnimation("SPELL1", owner)
 end
 
 function applyEffects()
@@ -18,17 +17,15 @@ function applyEffects()
     local damage = 25 + (spellLevel*55) + AP
     dealMagicalDamage(damage)
 
-    local target = getTarget()
-    local speed = projectileSpeed
-
-    if ( not isDead( target ) ) then
+    if not isDead( getTarget() ) then
+        addParticleTarget(owner, "Blitzcrank_Grapplin_tar.troy", getTarget(), 1, "L_HAND")
     	-- dash the user slightly in front of blitz
 	    local current = Vector2:new(owner.X, owner.Y)
 	    local to = (Vector2:new(spell.X, spell.Y) - current):normalize()
     	local range = to * 50
 	    local trueCoords = current + range
 
-    	dashToLocation( target, trueCoords.x, trueCoords.y, speed, true)
+    	dashToLocation( getTarget(), trueCoords.x, trueCoords.y, projectileSpeed, true)
     end
 
     destroyProjectile()
