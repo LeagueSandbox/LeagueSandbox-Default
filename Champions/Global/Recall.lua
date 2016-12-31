f2ab515b5e42923ff46811df6c8d9fe015fd1317
Sahin Habermann <sahino1996@hotmail.de>
2016-12-31 01:22:07 +0100

    Add Channeling functions to Recall
    for basic channeling on GameServer

--- Champions/Global/Recall.lua
+++ Champions/Global/Recall.lua
@@ -1,9 +1,22 @@
-function onStartCasting()
+Vector2 = require 'Vector2' -- include 2d vector lib 
+
+function onStartCasting()      
 end
 
 function onFinishCasting()
 	--addBuff("Recall", 8, owner, owner)
 	addParticleTarget(owner, "TeleportHome.troy", owner)
+end
+
+function onStartChanneling()
+  printChat("StartChanneling")
+end
+
+function onFinishChanneling()             
+  printChat("FinishChanneling")
+  local teamBlue = Vector2:new(26, 280)       
+  local teamPurple = Vector2:new(13927, 14175)      
+  teleport(owner, teamBlue.x, teamBlue.y)
 end
 
 function applyEffects()
