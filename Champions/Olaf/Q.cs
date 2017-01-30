7cbf865e04841c3ccc7ad3994c9d53196721ab89
xDawiss <dawis2121@gmail.com>
2017-01-30 19:59:28 +0100

    olaf-q working

--- Champions/Olaf/Q.cs
+++ Champions/Olaf/Q.cs
@@ -1,31 +1,48 @@
+using System.Numerics;
 using LeagueSandbox.GameServer.Logic.GameObjects;
-
+using LeagueSandbox.GameServer.Logic.API;
+ 
 namespace Olaf
- {
-     public class Q
-     {
-         public static void OnStartCasting(Champion owner, Spell spell, Unit target)
-         {
-
-        
-        
-         }
-         public static void OnFinishCasting(Champion owner, Spell spell, Unit target)
-         {
-
-        
-        
-         }
-         public static void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
-         {
-
-        
-        
-         }
-         public static void OnUpdate(double diff) {
-       
-        
-          
-         }
-     }
- }
+{
+    public class Q
+    {
+        public static void onStartCasting(Champion owner, Spell spell, Unit target)
+        {
+             
+        }
+ 
+        public static void onFinishCasting(Champion owner, Spell spell, Unit target)
+        {
+            var current = new Vector2(owner.X, owner.Y);
+            var to = new Vector2(spell.X, spell.Y) - current;
+            Vector2 trueCoords;
+ 
+            if (to.Length() > 1550)
+            {
+                to = Vector2.Normalize(to);
+                var range = to * 1550;
+                trueCoords = current + range;
+            }
+            else
+            {
+                trueCoords = new Vector2(spell.X, spell.Y);
+            }
+ 
+            spell.AddProjectile("OlafAxeThrowDamage", trueCoords.X, trueCoords.Y);
+        }
+ 
+        public static void applyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
+        {
+            ApiFunctionManager.AddParticleTarget(owner, "olaf_axeThrow_tar.troy", target, 1);
+            var AD = owner.GetStats().AttackDamage.Total * 1.1f;
+            var AP = owner.GetStats().AttackDamage.Total * 0.0f;
+            var damage = 15 + spell.Level * 20 + AD + AP;
+            owner.DealDamageTo(target, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
+        }
+ 
+        public static void onUpdate(double diff)
+        {
+ 
+        }
+    }
+}
