4783f0e7f83b0f7201ecd6b9f5b9f861e8f42fae
Wyyzapd <2417003090@qq.com>
2017-02-08 19:51:59 +0800

    Q.cs
    Removed lua and did minor changes

--- Champions/Kassadin/Q.cs
+++ Champions/Kassadin/Q.cs
@@ -1,21 +1,44 @@
 using LeagueSandbox.GameServer.Logic.GameObjects;
+using LeagueSandbox.GameServer.Logic.API;
 
-namespace Kassadin
+namespace kassadin
 {
     public class Q
     {
         public static void OnStartCasting(Champion owner, Spell spell, Unit target)
         {
-
+            ApiFunctionManager.AddParticleTarget(owner, "Kassadin_Base_cas.troy", owner, 1, "L_HAND");
         }
 
         public static void OnFinishCasting(Champion owner, Spell spell, Unit target)
         {
-
+            spell.AddProjectileTarget("NullLance", target, true);
         }
 
         public static void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
         {
+            var ap = owner.GetStats().AbilityPower.Total * 0.8f;
+            var damage = 30 + spell.Level * 50 + ap;
+
+            if (target != null && !ApiFunctionManager.IsDead(target))
+            {
+                owner.DealDamageTo(target, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+                if (target.IsDead)
+                {
+                    float manaToRecover = 60 + spell.Level * 10;
+                    var newMana = owner.GetStats().CurrentMana + manaToRecover;
+                    var maxMana = owner.GetStats().ManaPoints.Total;
+                    if (newMana >= maxMana)
+                    {
+                        owner.GetStats().CurrentMana = maxMana;
+                    }
+                    else
+                    {
+                        owner.GetStats().CurrentMana = newMana;
+                    }
+                }
+            }
+            projectile.setToRemove();
 
         }
 
