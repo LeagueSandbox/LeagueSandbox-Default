86bb00a073b5156eed044a214887e00a7f0d1ade
MatthewFrench <matthewf@ksu.edu>
2017-02-28 12:59:37 -0600

    Fix for summoner spells

--- Champions/Global/Recall.cs
+++ Champions/Global/Recall.cs
@@ -1,25 +1,35 @@
 using LeagueSandbox.GameServer.Logic.GameObjects;
 using LeagueSandbox.GameServer.Logic.API;
+using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
+
 namespace Global
 {
-    public static class Recall
+    public class Recall : GameScript
     {
-        public static void OnStartCasting(Champion owner, Spell spell, Unit target)
+        public void OnStartCasting(Champion owner, Spell spell, Unit target)
         {
         
         }
-        public static void OnFinishCasting(Champion owner, Spell spell, Unit target)
+        public void OnFinishCasting(Champion owner, Spell spell, Unit target)
         {
             //addBuff("Recall", 8, owner, owner)
             ApiFunctionManager.AddParticleTarget(owner, "TeleportHome.troy", owner);
         }
-        public static void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
+        public void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
         {
         
         }
-        public static void OnUpdate(double diff)
+        public void OnUpdate(double diff)
         {
-        
+
+        }
+
+        public void OnActivate(Champion owner)
+        {
+        }
+
+        public void OnDeactivate(Champion owner)
+        {
         }
     }
 }
