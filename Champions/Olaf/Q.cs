53a0f92fb5c31e37d23d2e619fc4b94866fc915d
xDawiss <dawis2121@gmail.com>
2017-03-09 19:50:59 +0100

    Q.cs
    Works in 50% (can throw and attack enemy but can't pick up axe to reset colldown)

--- Champions/Olaf/Q.cs
+++ Champions/Olaf/Q.cs
@@ -18,10 +18,29 @@ namespace Olaf
 
         }
         public void OnFinishCasting(Champion owner, Spell spell, Unit target) {
-
+            var current = new Vector2(owner.X, owner.Y);
+            var to = new Vector2(spell.X, spell.Y) - current;
+            Vector2 trueCoords;
+ 
+            if (to.Length() > 1650)
+            {
+                to = Vector2.Normalize(to);
+                var range = to * 1650;
+                trueCoords = current + range;
+            }
+            else
+            {
+                trueCoords = new Vector2(spell.X, spell.Y);
+            }
+ 
+            spell.AddProjectile("OlafAxeThrowDamage", trueCoords.X, trueCoords.Y);
         }
         public void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile) {
-
+            ApiFunctionManager.AddParticleTarget(owner, "olaf_axeThrow_tar.troy", target, 1);
+            var AD = owner.GetStats().AttackDamage.Total * 1.1f;
+            var AP = owner.GetStats().AttackDamage.Total * 0.0f;
+            var damage = 15 + spell.Level * 20 + AD + AP;
+            owner.DealDamageTo(target, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
         }
         public void OnUpdate(double diff) {
 
