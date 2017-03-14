27e895474091ea6520385777e3b3baf87f1126d8
MatthewFrench <matthewf@ksu.edu>
2017-03-14 12:47:10 -0600

    Fixed Damages on Garen E

--- Champions/Garen/E.cs
+++ Champions/Garen/E.cs
@@ -18,9 +18,7 @@ namespace Garen
         }
         public void OnStartCasting(Champion owner, Spell spell, Unit target)
         {
-            //ApiFunctionManager.AddParticleTarget(owner, "Garen_Base_E_Cas.troy", owner, 1, "Root");
             Particle p = ApiFunctionManager.AddParticleTarget(owner, "Garen_Base_E_Spin.troy", owner, 1);
-            //ApiFunctionManager.AddParticleTarget(owner, "Garen_Base_E_Cancel.troy", owner, 1);
             var visualBuff = ApiFunctionManager.AddBuffHUDVisual("GarenE", 3.0f, 1, owner);
             ApiFunctionManager.CreateTimer(3.0f, () =>
             {
@@ -28,18 +26,13 @@ namespace Garen
                 ApiFunctionManager.RemoveParticle(p);
             });
 
-            ApiFunctionManager.CreateTimer(1.0f, () =>
+            for (float i = 0.0f; i <= 3.0; i+= 0.5f)
             {
-                ApplySpinDamage(owner, spell, target);
-            });
-            ApiFunctionManager.CreateTimer(2.0f, () =>
-            {
-                ApplySpinDamage(owner, spell, target);
-            });
-            ApiFunctionManager.CreateTimer(3.0f, () =>
-            {
-                ApplySpinDamage(owner, spell, target);
-            });
+                ApiFunctionManager.CreateTimer(i, () =>
+                {
+                    ApplySpinDamage(owner, spell, target);
+                });
+            }
         }
         private void ApplySpinDamage(Champion owner, Spell spell, Unit target)
         {
@@ -48,10 +41,11 @@ namespace Garen
             {
                 if (unit.Team != owner.Team)
                 {
-                    var ad = new[] { .7f, .8f, .9f, 1f, 1.1f }[spell.Level - 1] * owner.GetStats().AttackDamage.Total;
-                    var damage = -5 + spell.Level * 25 + ad;
+                    //PHYSICAL DAMAGE PER SECOND: 20 / 45 / 70 / 95 / 120 (+ 70 / 80 / 90 / 100 / 110% AD)
+                    float ad = new[] { .7f, .8f, .9f, 1f, 1.1f }[spell.Level - 1] * owner.GetStats().AttackDamage.Total * 0.5f;
+                    float damage = new[] { 20, 45, 70, 95, 120 }[spell.Level - 1] * 0.5f + ad;
                     if (unit is Minion) damage *= 0.75f;
-                    owner.DealDamageTo(unit, damage * 1, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+                    owner.DealDamageTo(unit, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
                 }
             }
         }
