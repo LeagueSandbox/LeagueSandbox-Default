5500a534d598bafee9d11b565a3f55c63f129847
MatthewFrench <matthewf@ksu.edu>
2017-03-14 12:28:20 -0600

    Garen spin is mostly working

--- Champions/Garen/E.cs
+++ Champions/Garen/E.cs
@@ -18,15 +18,32 @@ namespace Garen
         }
         public void OnStartCasting(Champion owner, Spell spell, Unit target)
         {
-            ApiFunctionManager.AddParticleTarget(owner, "Garen_Base_E_Cas.troy", owner, 1, "Root");
-            ApiFunctionManager.AddParticleTarget(owner, "Garen_Base_E_Spin.troy", owner, 1);
-            ApiFunctionManager.AddParticleTarget(owner, "Garen_Base_E_Cancel.troy", owner, 1);
+            //ApiFunctionManager.AddParticleTarget(owner, "Garen_Base_E_Cas.troy", owner, 1, "Root");
+            Particle p = ApiFunctionManager.AddParticleTarget(owner, "Garen_Base_E_Spin.troy", owner, 1);
+            //ApiFunctionManager.AddParticleTarget(owner, "Garen_Base_E_Cancel.troy", owner, 1);
             var visualBuff = ApiFunctionManager.AddBuffHUDVisual("GarenE", 3.0f, 1, owner);
             ApiFunctionManager.CreateTimer(3.0f, () =>
             {
                 ApiFunctionManager.RemoveBuffHUDVisual(visualBuff);
+                ApiFunctionManager.RemoveParticle(p);
             });
-            List<Unit> units = ApiFunctionManager.GetUnitsInRange(target, 500, true);
+
+            ApiFunctionManager.CreateTimer(1.0f, () =>
+            {
+                ApplySpinDamage(owner, spell, target);
+            });
+            ApiFunctionManager.CreateTimer(2.0f, () =>
+            {
+                ApplySpinDamage(owner, spell, target);
+            });
+            ApiFunctionManager.CreateTimer(3.0f, () =>
+            {
+                ApplySpinDamage(owner, spell, target);
+            });
+        }
+        private void ApplySpinDamage(Champion owner, Spell spell, Unit target)
+        {
+            List<Unit> units = ApiFunctionManager.GetUnitsInRange(owner, 500, true);
             foreach (Unit unit in units)
             {
                 if (unit.Team != owner.Team)
@@ -34,24 +51,16 @@ namespace Garen
                     var ad = new[] { .7f, .8f, .9f, 1f, 1.1f }[spell.Level - 1] * owner.GetStats().AttackDamage.Total;
                     var damage = -5 + spell.Level * 25 + ad;
                     if (unit is Minion) damage *= 0.75f;
-                    owner.DealDamageTo(target, damage * 1, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
-
-
+                    owner.DealDamageTo(unit, damage * 1, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
                 }
             }
         }
+
         public void OnFinishCasting(Champion owner, Spell spell, Unit target)
         {
-            var unitsHit = ApiFunctionManager.GetUnitsInRange(owner, 175, true);
-
         }
         public void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
         {
-            var ad = new[] { .7f, .8f, .9f, 1f, 1.1f }[spell.Level - 1] * owner.GetStats().AttackDamage.Total;
-            var damage = -5 + spell.Level * 25 + ad;
-            if (target is Minion)
-                damage *= 0.75f;
-            owner.DealDamageTo(target, damage * 1, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
         }
         public void OnUpdate(double diff)
         {
