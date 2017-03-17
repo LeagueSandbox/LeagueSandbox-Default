12d7e65610354b28499b7efd9aabeb31e71bdc4d
xDawiss <dawis2121@gmail.com>
2017-03-17 18:29:31 +0100

    Recall teleport working.

--- Champions/Global/Recall.cs
+++ Champions/Global/Recall.cs
@@ -1,23 +1,35 @@
-using LeagueSandbox.GameServer.Logic.GameObjects;
+using LeagueSandbox.GameServer.Core.Logic.PacketHandlers;
+using LeagueSandbox.GameServer.Core.Logic.RAF;
+using LeagueSandbox.GameServer.Logic;
 using LeagueSandbox.GameServer.Logic.API;
+using LeagueSandbox.GameServer.Logic.Enet;
+using LeagueSandbox.GameServer.Logic.GameObjects;
+using LeagueSandbox.GameServer.Logic.Packets;
 using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
+using System.Collections.Generic;
 
 namespace Global
 {
     public class Recall : GameScript
     {
+
         public void OnStartCasting(Champion owner, Spell spell, Unit target)
         {
-        
+
         }
         public void OnFinishCasting(Champion owner, Spell spell, Unit target)
         {
-            //addBuff("Recall", 8, owner, owner)
-            ApiFunctionManager.AddParticleTarget(owner, "TeleportHome.troy", owner);
+            var visualBuff = ApiFunctionManager.AddBuffHUDVisual("Recall", 8.0f, 1, owner);
+            var AddParticle = ApiFunctionManager.AddParticleTarget(owner, "TeleportHome.troy", owner);
+            ApiFunctionManager.CreateTimer(8.0f, () =>
+            {
+                ApiFunctionManager.RemoveBuffHUDVisual(visualBuff);
+                owner.Recall(owner);
+            });
         }
         public void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
         {
-        
+
         }
         public void OnUpdate(double diff)
         {
