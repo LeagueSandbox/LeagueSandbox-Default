9427cab341f05b0b726c72d9088aa19cc16569df
DeudlyYT <deudlymandame@gmail.com>
2017-03-17 08:43:22 +0100

    Teemo Q and Blind buff

--- Champions/Teemo/Q.cs
+++ Champions/Teemo/Q.cs
@@ -14,14 +14,30 @@ namespace Teemo
     {
         public void OnActivate(Champion owner) { }
         public void OnDeactivate(Champion owner) { }
-        public void OnStartCasting(Champion owner, Spell spell, Unit target){
-
+        public void OnStartCasting(Champion owner, Spell spell, Unit target)
+        {
+            float time = 1.25f + 0.25f * spell.Level;
+            var buff = target.AddBuffGameScript("Blind", "Blind", spell);
+            var visualBuff = ApiFunctionManager.AddBuffHUDVisual("Blind", time, 1, target);
+            ApiFunctionManager.CreateTimer(time, () =>
+            {
+                ApiFunctionManager.RemoveBuffHUDVisual(visualBuff);
+                target.RemoveBuffGameScript(buff);
+            });
         }
         public void OnFinishCasting(Champion owner, Spell spell, Unit target) {
+            var current = new Vector2(owner.X, owner.Y);
+            var to = Vector2.Normalize(new Vector2(spell.X, spell.Y) - current);
+            var range = to * 580;
+            var trueCoords = current + range;
 
+            spell.AddProjectile("ToxicShot", trueCoords.X, trueCoords.Y);
         }
-        public void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile) {
-
+        public void ApplyEffects(Champion owner, Unit target, Spell spell, Projectile projectile)
+        {
+            var ap = owner.GetStats().AbilityPower.Total * 0.8f;
+            var damage = 35 + spell.Level * 45 + ap;
+            owner.DealDamageTo(target, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
         }
         public void OnUpdate(double diff) {
 
