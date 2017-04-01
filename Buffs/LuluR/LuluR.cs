90dcabc4378614d671dc57106285aabdca49cd61
DeudlyYT <deudlymandame@gmail.com>
2017-04-01 14:45:31 +0200

    Added all the missing code of LuluR and its buff

--- Buffs/LuluR/LuluR.cs
+++ Buffs/LuluR/LuluR.cs
@@ -12,12 +12,18 @@ namespace LuluR
         public void OnActivate(Unit unit, Spell ownerSpell)
         {
             statMod.Size.PercentBonus = statMod.Size.PercentBonus + 1;
+            statMod.HealthPoints.BaseBonus = statMod.HealthPoints.BaseBonus + 150 + 150 * ownerSpell.Level;
+            unit.GetStats().CurrentHealth = unit.GetStats().CurrentHealth + 150 + 150 * ownerSpell.Level;
             unit.AddStatModifier(statMod);
         }
 
         public void OnDeactivate(Unit unit)
         {
             unit.RemoveStatModifier(statMod);
+            if (unit.GetStats().CurrentHealth > unit.GetStats().HealthPoints.Total)
+            {
+                unit.GetStats().CurrentHealth = unit.GetStats().HealthPoints.Total;
+            }
         }
 
         public void OnUpdate(double diff)
