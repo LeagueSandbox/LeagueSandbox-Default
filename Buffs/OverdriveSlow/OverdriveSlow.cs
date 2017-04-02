ea4c0c1db928b1786d71ae311599e917d9a83a45
DeudlyYT <deudlymandame@gmail.com>
2017-04-02 20:09:42 +0200

    Some buffs and spells

---
+++ Buffs/OverdriveSlow/OverdriveSlow.cs
@@ -0,0 +1,28 @@
+using System;
+using LeagueSandbox.GameServer.Logic.GameObjects;
+using LeagueSandbox.GameServer.Logic.Scripting;
+using LeagueSandbox.GameServer.Logic.API;
+
+namespace OverdriveSlow
+{
+    class OverdriveSlow : BuffGameScript
+    {
+        ChampionStatModifier statMod = new ChampionStatModifier();
+
+        public void OnActivate(Unit unit, Spell ownerSpell)
+        {
+            statMod.MoveSpeed.PercentBonus = statMod.MoveSpeed.PercentBonus - 0.3f;
+            unit.AddStatModifier(statMod);
+        }
+
+        public void OnDeactivate(Unit unit)
+        {
+            unit.RemoveStatModifier(statMod);
+        }
+
+        public void OnUpdate(double diff)
+        {
+            
+        }
+    }
+}
