8f2e1f8406f9601ff45c2a0feb3353f14190515b
MatthewFrench <matthewf@ksu.edu>
2017-04-29 17:33:26 -0500

    Added movement too Poro

--- Behavior/Poro/Poro.cs
+++ Behavior/Poro/Poro.cs
@@ -1,7 +1,9 @@
-﻿using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
+﻿using LeagueSandbox.GameServer.Logic.GameObjects;
+using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,13 @@ namespace Behavior
 {
     class Poro : IGameScript
     {
+        Unit selfUnit;
         public void OnActivate(GameScriptInformation scriptInfo)
         {
+            selfUnit = scriptInfo.TargetUnit;
             Console.WriteLine("Poro activated");
+            selfUnit.GetStats().MoveSpeed.BaseValue = 300;
+            selfUnit.SetWaypoints(new List<Vector2> { new Vector2(selfUnit.X, selfUnit.Y),  new Vector2(selfUnit.X + 50000, selfUnit.Y) });
         }
 
         public void OnDeactivate()
