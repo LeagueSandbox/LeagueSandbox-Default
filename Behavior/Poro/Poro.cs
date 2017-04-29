b4208a7fbc212ce254ecae9d04d92837984bad81
MatthewFrench <matthewf@ksu.edu>
2017-04-29 14:22:53 -0500

    Added poro behavior script

---
+++ Behavior/Poro/Poro.cs
@@ -0,0 +1,21 @@
+﻿using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavior
+{
+    class Poro : IGameScript
+    {
+        public void OnActivate(GameScriptInformation scriptInfo)
+        {
+            Console.WriteLine("Poro activated");
+        }
+
+        public void OnDeactivate()
+        {
+        }
+    }
+}
