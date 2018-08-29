94489853423f71d2e7e5af9c137162cdcb387ba6
Philippe <lipeph99@gmail.com>
2018-08-29 14:20:56 -0300

    Update R.cs

--- Champions/MasterYi/R.cs
+++ Champions/MasterYi/R.cs
@@ -7,45 +7,45 @@ using LeagueSandbox.GameServer.Scripting.CSharp;
 
 namespace Spells
 {
-	public class MasterYiHighlander : IGameScript
-	{
-		public void OnActivate(Champion owner)
-		{
-		}
+    public class MasterYiHighlander : IGameScript
+    {
+        public void OnActivate(Champion owner)
+        {
+        }
 
-		private void ReduceCooldown(AttackableUnit unit, bool isCrit)
-		{
-		//No Cooldown reduction on the other skills yet
-		}
+        private void ReduceCooldown(AttackableUnit unit, bool isCrit)
+        {
+        //No Cooldown reduction on the other skills yet
+        }
 
-		public void OnDeactivate(Champion owner)
-		{
-		}
+        public void OnDeactivate(Champion owner)
+        {
+        }
 
-		public void OnStartCasting(Champion owner, Spell spell, AttackableUnit target)
-		{
-			var p = ApiFunctionManager.AddParticleTarget(owner, "Highlander_buf.troy", target, 1);
-			var buff = ((ObjAiBase) target).AddBuffGameScript("Highlander", "Highlander", spell);
-			var visualBuff = ApiFunctionManager.AddBuffHudVisual("Highlander", 10.0f, 1, owner);
-			ApiFunctionManager.CreateTimer(10.0f, () =>
-			{
-				ApiFunctionManager.RemoveParticle(p);
-				ApiFunctionManager.RemoveBuffHudVisual(visualBuff);
-				((ObjAiBase) target).RemoveBuffGameScript(buff);
-			});
-			//No increased durations on kills and assists yet
-		}
+        public void OnStartCasting(Champion owner, Spell spell, AttackableUnit target)
+        {
+            var p = ApiFunctionManager.AddParticleTarget(owner, "Highlander_buf.troy", target, 1);
+            var buff = ((ObjAiBase) target).AddBuffGameScript("Highlander", "Highlander", spell);
+            var visualBuff = ApiFunctionManager.AddBuffHudVisual("Highlander", 10.0f, 1, owner);
+            ApiFunctionManager.CreateTimer(10.0f, () =>
+            {
+                ApiFunctionManager.RemoveParticle(p);
+                ApiFunctionManager.RemoveBuffHudVisual(visualBuff);
+                ((ObjAiBase) target).RemoveBuffGameScript(buff);
+            });
+            //No increased durations on kills and assists yet
+        }
 
-		public void OnFinishCasting(Champion owner, Spell spell, AttackableUnit target)
-		{
-		}
+        public void OnFinishCasting(Champion owner, Spell spell, AttackableUnit target)
+        {
+        }
 
-		public void ApplyEffects(Champion owner, AttackableUnit target, Spell spell, Projectile projectile)
-		{
-		}
+        public void ApplyEffects(Champion owner, AttackableUnit target, Spell spell, Projectile projectile)
+        {
+        }
 
-		public void OnUpdate(double diff)
-		{
-		}
-	}
+        public void OnUpdate(double diff)
+        {
+        }
+    }
 }
