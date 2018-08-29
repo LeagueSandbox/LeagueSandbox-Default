da5eda82dd27115f2a575b19e2bd79e7579aacff
Philippe <lipeph99@gmail.com>
2018-08-29 14:19:49 -0300

    Update Highlander.cs

--- Buffs/Highlander/Highlander.cs
+++ Buffs/Highlander/Highlander.cs
@@ -5,30 +5,30 @@ using LeagueSandbox.GameServer.Scripting.CSharp;
 
 namespace Highlander
 {
-	internal class Highlander : IBuffGameScript
-	{
-		private StatsModifier _statMod;
+    internal class Highlander : IBuffGameScript
+    {
+        private StatsModifier _statMod;
 
-		public void OnActivate(ObjAiBase unit, Spell ownerSpell)
-		{
-			_statMod = new StatsModifier();
-			_statMod.MoveSpeed.PercentBonus = _statMod.MoveSpeed.PercentBonus + (15f + ownerSpell.Level * 10) / 100f;
-			_statMod.AttackSpeed.PercentBonus = _statMod.AttackSpeed.PercentBonus + (5f + ownerSpell.Level * 25) / 100f;
-			unit.AddStatModifier(_statMod);
-			//Immunity to slowness not added
-		}
+        public void OnActivate(ObjAiBase unit, Spell ownerSpell)
+        {
+            _statMod = new StatsModifier();
+            _statMod.MoveSpeed.PercentBonus = _statMod.MoveSpeed.PercentBonus + (15f + ownerSpell.Level * 10) / 100f;
+            _statMod.AttackSpeed.PercentBonus = _statMod.AttackSpeed.PercentBonus + (5f + ownerSpell.Level * 25) / 100f;
+            unit.AddStatModifier(_statMod);
+            //Immunity to slowness not added
+        }
 
-		public void OnDeactivate(ObjAiBase unit)
-		{
-			unit.RemoveStatModifier(_statMod);
-		}
+        public void OnDeactivate(ObjAiBase unit)
+        {
+            unit.RemoveStatModifier(_statMod);
+        }
 
-		private void OnAutoAttack(AttackableUnit target, bool isCrit)
-		{
-		}
+        private void OnAutoAttack(AttackableUnit target, bool isCrit)
+        {
+        }
 
-		public void OnUpdate(double diff)
-		{
-		}
-	}
+        public void OnUpdate(double diff)
+        {
+        }
+    }
 }
