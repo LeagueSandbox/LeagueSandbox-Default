2fbaf6b46e2fb33e1a2850328041edc0dbc394ab
Menderes <mehmet41650@gmail.com>
2018-11-01 03:18:21 +0300

    Root has been added

---
+++ Buffs/Root/Root.cs
@@ -0,0 +1,27 @@
+﻿using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.Spells;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+
+namespace Root
+{
+    internal class Root : IBuffGameScript
+    {
+        private UnitCrowdControl _crowd = new UnitCrowdControl(CrowdControlType.ROOT);
+
+        public void OnActivate(ObjAiBase unit, Spell ownerSpell)
+        {
+            unit.ApplyCrowdControl(_crowd);
+        }
+
+        public void OnDeactivate(ObjAiBase unit)
+        {
+            unit.RemoveCrowdControl(_crowd);
+        }
+
+        public void OnUpdate(double diff)
+        {
+
+        }
+    }
+}
