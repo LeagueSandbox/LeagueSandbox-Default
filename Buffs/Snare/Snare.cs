e7c22d170374cb01b6f4f6c32fb306accc08e638
Menderes <mehmet41650@gmail.com>
2019-02-03 17:28:36 +0300

    snare has added

---
+++ Buffs/Snare/Snare.cs
@@ -0,0 +1,28 @@
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.Spells;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+
+namespace Snare
+{
+    internal class Disarm : IBuffGameScript
+    {
+        private UnitCrowdControl _crowd = new UnitCrowdControl(CrowdControlType.SNARE);
+
+        public void OnActivate(ObjAiBase unit, Spell ownerSpell)
+        {
+            unit.ApplyCrowdControl(_crowd);
+        }
+
+        public void OnDeactivate(ObjAiBase unit)
+        {
+            unit.RemoveCrowdControl(_crowd);
+        }
+
+        public void OnUpdate(double diff)
+        {
+
+        }
+    }
+}
+
