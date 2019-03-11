58f13c0c364f51e81afebee0066c90380958c61e
xxx3456789 <46378803+xxx3456789@users.noreply.github.com>
2019-03-11 22:48:50 +0800

    another way of Taric W
    use target.Stats.~~ instead of StatModifier

--- Champions/Taric/W.cs
+++ Champions/Taric/W.cs
@@ -6,39 +6,33 @@ using GameServerCore.Enums;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
-using LeagueSandbox.GameServer.GameObjects.Stats;
-
 
 namespace Spells
 {
     public class Shatter : IGameScript
     {
-        public StatsModifier _statMod;
-
         public void OnActivate(IChampion owner)
         {
-            
+
         }
-       
+
         public void OnDeactivate(IChampion owner)
         {
-            
+
         }
 
         public void OnStartCasting(IChampion owner, ISpell spell, IAttackableUnit target)
         {
-            
+
         }
 
         public void OnFinishCasting(IChampion owner, ISpell spell, IAttackableUnit target)
         {
-            _statMod = new StatsModifier();
             var armor = owner.Stats.Armor.Total;
             var damage = spell.Level * 40 + armor * 0.2f;
             var reduce = spell.Level * 5 + armor * 0.05f;
             var p1 = AddParticleTarget(owner, "Shatter_nova.troy", owner, 1);
-            _statMod.Armor.FlatBonus -= reduce;
-            
+
             foreach (var enemys in GetUnitsInRange(owner, 375, true)
                 .Where(x => x.Team == CustomConvert.GetEnemyTeam(owner.Team)))
             {
@@ -55,12 +49,12 @@ namespace Spells
                     }
                     if (hasbuff == false)
                     {
-                        ((ObjAiBase)enemys).AddStatModifier(_statMod);                        
+                        enemys.Stats.Armor.FlatBonus -= reduce;
                     }
 
                     CreateTimer(4f, () =>
                     {
-                        ((ObjAiBase)enemys).RemoveStatModifier(_statMod);
+                        enemys.Stats.Armor.FlatBonus += reduce;
                         RemoveParticle(p2);
                     });
                 }
@@ -69,12 +63,12 @@ namespace Spells
 
         public void ApplyEffects(IChampion owner, IAttackableUnit target, ISpell spell, IProjectile projectile)
         {
-         
+
         }
 
         public void OnUpdate(double diff)
         {
-            
+
         }
     }
 }
