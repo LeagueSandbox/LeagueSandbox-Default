0698c729f9e9b882994e4f808b6100fa08e333b6
xxx3456789 <46378803+xxx3456789@users.noreply.github.com>
2019-05-07 22:22:14 +0800

    Update R.cs
    IAttackableUnit include turrets/inhibitor/nexus, and it will cause server crash.
    fixed.

--- Champions/Taric/R.cs
+++ Champions/Taric/R.cs
@@ -35,7 +35,7 @@ namespace Spells
             foreach (var enemyTarget in GetUnitsInRange(owner, 375, true)
                 .Where(x => x.Team == CustomConvert.GetEnemyTeam(owner.Team)))
             {
-                if (owner.Team != enemyTarget.Team && enemyTarget is IAttackableUnit)
+                if (enemyTarget is IChampion || enemyTarget is IMinion && owner.Team != enemyTarget.Team)
                 {
                     enemyTarget.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL,false);
                     var ep1 = AddParticleTarget(owner, "Taric_GemStorm_Tar.troy", enemyTarget, 1.25f);
@@ -53,7 +53,7 @@ namespace Spells
             foreach (var allyTarget in GetUnitsInRange(owner, 1100, true)
                 .Where(x => x.Team != CustomConvert.GetEnemyTeam(owner.Team)))
             {
-                if (owner.Team == allyTarget.Team && owner != allyTarget && hasbuff == false && allyTarget is IAttackableUnit)
+                if (allyTarget is IChampion || allyTarget is IMinion && owner.Team == allyTarget.Team && owner != allyTarget && hasbuff == false)
                 {
                     ((ObjAiBase)allyTarget).AddBuffGameScript("Radiance_ally", "Radiance_ally", spell, 10.0f, true);
                 }
