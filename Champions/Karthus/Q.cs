9f424018c0edca4fc504d845d95206c1f781158c
MingSaber976 <48879549+MingSaber976@users.noreply.github.com>
2019-05-21 21:04:02 +0800

    Update Q.cs
    1.Change `spellPosition` to `affectUnits`
    2.change the name `units` to `unit`
    3.make codes into `OnFinishChasting`
    4.let `affectedUnits.Count == 0` part before the `foreach()`

--- Champions/Karthus/Q.cs
+++ Champions/Karthus/Q.cs
@@ -22,43 +22,39 @@ namespace Spells
             AddParticle(owner, "Karthus_Base_Q_Point.troy", spell.X, spell.Y);
             AddParticle(owner, "Karthus_Base_Q_Ring.troy", spell.X, spell.Y);
             AddParticle(owner, "Karthus_Base_Q_Skull_Child.troy", spell.X, spell.Y);
-            CreateTimer(0.5f, () =>
-            {
-                IGameObject m = AddParticle(owner, "Karthus_Base_Q_Explosion.troy", spell.X, spell.Y);
-                var spellPosition = GetUnitsInRange(m, 150, true);
+        }   
+
+        public void OnFinishCasting(IChampion owner, ISpell spell, IAttackableUnit target)
+        {
+            IGameObject m = AddParticle(owner, "Karthus_Base_Q_Explosion.troy", spell.X, spell.Y);
+                var affectedUnits = GetUnitsInRange(m, 150, true);
                 var ap = owner.Stats.AbilityPower.Total;
                 var damage = 20f + spell.Level * 20f + ap * 0.3f;
-                
-                foreach (var units in spellPosition
+                if (affectedUnits.Count == 0)
+                {
+                AddParticle(owner, "Karthus_Base_Q_Hit_Miss.troy", spell.X, spell.Y);
+                }
+                        
+                foreach (var unit in affectedUnits
                 .Where(x => x.Team == CustomConvert.GetEnemyTeam(owner.Team)))
                 {
-                    if (units is IChampion || units is IMinion)
-                    {
-                        if (spellPosition.Count == 0)
-                        {
-                            AddParticle(owner, "Karthus_Base_Q_Hit_Miss.troy", spell.X, spell.Y);
-                        }
-                        if (spellPosition.Count == 1)
+                    if (unit is IChampion || unit is IMinion)
+                    {                        
+                        if (affectedUnits.Count == 1)
                         {
                             damage *= 2;
                             AddParticle(owner, "Karthus_Base_Q_Hit_Single.troy", spell.X, spell.Y);
-                            units.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, true);
+                            unit.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, true);
                         }
-                        if (spellPosition.Count > 1)
+                        if (affectedUnits.Count > 1)
                         {
                             AddParticle(owner, "Karthus_Base_Q_Hit_Many.troy", spell.X, spell.Y);
-                            units.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+                            unit.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
                         }
                     }
                 }
                 m.SetToRemove();
                 AddParticle(owner, "Karthus_Base_Q_Explosion_Sound.troy", spell.X, spell.Y);
-            });
-        }   
-
-        public void OnFinishCasting(IChampion owner, ISpell spell, IAttackableUnit target)
-        {
-            
         }
 
         public void ApplyEffects(IChampion owner, IAttackableUnit target, ISpell spell, IProjectile projectile)
