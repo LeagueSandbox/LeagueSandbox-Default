cfaac7e40b219f548c83e0a63c06435063af6228
Kamil <32514182+xTeJk@users.noreply.github.com>
2019-05-31 13:56:56 +0200

    Gangplank's E BUFF update

--- Buffs/GangplankE/GangplankE.cs
+++ Buffs/GangplankE/GangplankE.cs
@@ -3,22 +3,36 @@ using LeagueSandbox.GameServer.Scripting.CSharp;
 
 namespace GangplankE
 {
-    internal class GangplankE : BuffGameScript
+    internal class GangplankE : IBuffGameScript
     {
         private ChampionStatModifier _statMod;
+        private IBuff _hudvisual;
 
-        public void OnActivate(ObjAIBase unit, Spell ownerSpell)
+        public void OnActivate(IObjAIBase unit, ISpell ownerSpell)
         {
             _statMod = new ChampionStatModifier();
             _statMod.AttackSpeed.PercentBonus = _statMod.AttackSpeed.PercentBonus + (10f + 20f * ownerSpell.Level) / 100f;
             _statMod.MoveSpeed.PercentBonus = _statMod.MoveSpeed.PercentBonus + (10f + 5f * ownerSpell.Level) / 100f;
             _statMod.AttackDamage.PercentBonus = _statMod.AttackDamage.PercentBonus + (10f + 10f * ownerSpell.Level) / 100f;
             unit.AddStatModifier(_statMod);
+            
+            var time = 7.0f;
+            _hudvisual = AddBuffHUDVisual("RaiseMorale", time, 1, unit);
+            
+            Particle p_ally1 = AddParticleTarget(owner, "pirate_raiseMorale_cas.troy", unit, 1);
+            Particle p_ally2 = AddParticleTarget(owner, "pirate_raiseMorale_mis.troy", unit, 1);
+            Particle p_ally3 = AddParticleTarget(owner, "pirate_raiseMorale_tar.troy", unit, 1);
         }
 
         public void OnDeactivate(ObjAIBase unit)
         {
             unit.RemoveStatModifier(_statMod);
+            
+            RemoveBuffHudVisual(_hudvisual);
+            
+            RemoveParticle(p_ally1);
+            RemoveParticle(p_ally2);
+            RemoveParticle(p_ally3);
         }
 
         public void OnUpdate(double diff)
