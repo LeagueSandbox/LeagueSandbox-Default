eb4f59144c57c4a3b030e6ef2dc235e300f0ea4c
Kamil <32514182+xTeJk@users.noreply.github.com>
2019-05-31 14:45:35 +0200

    Gangplank's E update

--- Champions/Gangplank/E.cs
+++ Champions/Gangplank/E.cs
@@ -29,11 +29,11 @@ namespace Spells
 
         public void OnFinishCasting(IChampion owner, ISpell spell, IAttackableUnit target)
         {		
-            Particle p = AddParticleTarget(owner, "pirate_raiseMorale_cas.troy", target, 1);
-            Particle p2 = AddParticleTarget(owner, "pirate_raiseMorale_mis.troy", target, 1);
-            Particle p3 = AddParticleTarget(owner, "pirate_raiseMorale_tar.troy", target, 1);
-            var buff = ((ObjAIBase) target).AddBuffGameScript("GangplankE", "GangplankE", spell);
-            var visualBuff = AddBuffHUDVisual("RaiseMorale", 7.0f, 1, owner); // add hud visual
+            //Particle p = AddParticleTarget(owner, "pirate_raiseMorale_cas.troy", target, 1);
+            //Particle p2 = AddParticleTarget(owner, "pirate_raiseMorale_mis.troy", target, 1);
+            //Particle p3 = AddParticleTarget(owner, "pirate_raiseMorale_tar.troy", target, 1);
+            //var buff = ((ObjAIBase) target).AddBuffGameScript("GangplankE", "GangplankE", spell);
+            //var visualBuff = AddBuffHUDVisual("RaiseMorale", 7.0f, 1, owner); // add hud visual
             
             var hasbuff = owner.HasBuffGameScriptActive("GangplankE", "GangplankE");
             
@@ -59,18 +59,18 @@ namespace Spells
             }
             
             
-            CreateTimer(7.0f, () => // remove particle and buff (include hud visual) after 7 seconds
-            {
-                RemoveParticle(p);
-                RemoveParticle(p2);
-                RemoveParticle(p3);
-                //RemoveParticle(p_ally1);
-                //RemoveParticle(p_ally2);
-                //RemoveParticle(p_ally3);
-                RemoveBuffHUDVisual(visualBuff);
-                //RemoveBuffHUDVisual(visualBuffally);
-                owner.RemoveBuffGameScript(buff);
-            });				
+            //CreateTimer(7.0f, () => // remove particle and buff (include hud visual) after 7 seconds
+            //{
+            //    RemoveParticle(p);
+            //    RemoveParticle(p2);
+            //    RemoveParticle(p3);
+            //    //RemoveParticle(p_ally1);
+            //    //RemoveParticle(p_ally2);
+            //    //RemoveParticle(p_ally3);
+            //    RemoveBuffHUDVisual(visualBuff);
+            //    //RemoveBuffHUDVisual(visualBuffally);
+            //    owner.RemoveBuffGameScript(buff);
+            //});				
         }
 
         public void ApplyEffects(IChampion owner, IAttackableUnit target, ISpell spell, IProjectile projectile)
