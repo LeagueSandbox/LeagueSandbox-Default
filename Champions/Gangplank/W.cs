ba72e50f36293248a3994886e61e1b7edb2984b6
Kamil <32514182+xTeJk@users.noreply.github.com>
2019-05-31 04:28:26 +0200

    Update W.cs

--- Champions/Gangplank/W.cs
+++ Champions/Gangplank/W.cs
@@ -1,7 +1,7 @@
-using LeagueSandbox.GameServer.Logic.GameObjects;
-using LeagueSandbox.GameServer.Logic.API;
-using LeagueSandbox.GameServer.Logic.GameObjects.AttackableUnits;
-using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
+using GameServerCore.Domain.GameObjects;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.Scripting.CSharp;
 
 namespace Spells
 {
@@ -22,7 +22,11 @@ namespace Spells
         }
 
         public void OnStartCasting(Champion owner, Spell spell, AttackableUnit target)
-        {
+        {	
+        }
+
+        public void OnFinishCasting(Champion owner, Spell spell, AttackableUnit target)
+        {	
             float ad = owner.GetStats().AttackDamage.Total * 0.1f;
             owner.RestoreHealth(15*ad);
             var buff = ((ObjAIBase) target).AddBuffGameScript("GangplankW", "GangplankW", spell);
@@ -31,10 +35,6 @@ namespace Spells
                 //ApiFunctionManager.RemoveBuffHUDVisual(visualBuff);
                 owner.RemoveBuffGameScript(buff);
             });				
-        }
-
-        public void OnFinishCasting(Champion owner, Spell spell, AttackableUnit target)
-        {		
         }
 
         public void ApplyEffects(Champion owner, AttackableUnit target, Spell spell, Projectile projectile)
