6af4084dc7671b6ef531a57020e53935408d67d2
Kamil <32514182+xTeJk@users.noreply.github.com>
2019-05-31 04:39:44 +0200

    Update GangplankW.cs

--- Buffs/GangplankW/GangplankW.cs
+++ Buffs/GangplankW/GangplankW.cs
@@ -1,2 +1,27 @@
-
+using GameServerCore.Domain.GameObjects;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+
+namespace GangplankW
+{
+    internal class GangplankW : BuffGameScript
+    {
+        private ChampionStatModifier _statMod;
+
+        public void OnActivate(ObjAIBase unit, Spell ownerSpell)
+        {
+            _statMod = new ChampionStatModifier();
+            _statMod.MoveSpeed.PercentBonus = _statMod.MoveSpeed.PercentBonus + (10f + 5f * ownerSpell.Level) / 100f;
+            unit.AddStatModifier(_statMod);
+        }
+
+        public void OnDeactivate(ObjAIBase unit)
+        {
+            unit.RemoveStatModifier(_statMod);
+        }
+
+        public void OnUpdate(double diff)
+        {
+
+        }
+    }
+}
